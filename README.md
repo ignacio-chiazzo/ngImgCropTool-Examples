fc039c68394a625b03475eaf89adfd20c24eb43a
Ïgnacio <ignaciochiazzo@gmail.com>
2016-04-10 13:14:01 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# ngImgCropTool | Angular JS.
+
+It is an example of how to use ngImgCropTool Angular JS.
+
+## Getting started :
+	1) Clone the project
+	2) Open index.html in you r browser
+
+
+You can find the example in codepen : http://codepen.io/ignacio-chiazzo/pen/QNQyRW
+ 
--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ angular.module('app', ['ngImgCrop'])
   $scope.myCroppedImage='';
   $scope.filename = '';
   $scope.availableOptions= [
-  {id: '1', name: 'Microsoft (200*150)', value:{w: 200, h: 150}},
-  {id: '2', name: 'Macquarie (150*100)', value:{w: 150, h: 100}},
-  {id: '2', name: 'Macquarie (100*50)', value:{w: 100, h: 50}},
-  {id: '2', name: 'Macquarie (40*20)', value:{w: 40, h: 20}},
-  {id: '3', name: 'Option C (14*10)', value:{w: 15, h: 10}}
+  {id: '1', name: 'Option A (200*150)', value:{w: 200, h: 150}},
+  {id: '2', name: 'Option B (150*100)', value:{w: 150, h: 100}},
+  {id: '2', name: 'Option C (100*50)', value:{w: 100, h: 50}},
+  {id: '2', name: 'Option D (40*20)', value:{w: 40, h: 20}},
+  {id: '3', name: 'Option E (14*10)', value:{w: 15, h: 10}}
   ];
 
   $scope.selectedOption = $scope.availableOptions[1];
