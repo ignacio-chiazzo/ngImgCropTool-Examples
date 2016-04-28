7d1a22495b90cb905f2ac15f8ec1ab04f752b12c
Geore Polzer <gpolzer@yourbizzwizz.com>
2016-04-28 16:49:47 -0700

    I am suggesting you to change index.jx

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ angular.module('app', ['ngImgCrop'])
   {id: '2', name: 'Option B (150*100)', value:{w: 150, h: 100}},
   {id: '2', name: 'Option C (100*50)', value:{w: 100, h: 50}},
   {id: '2', name: 'Option D (40*20)', value:{w: 40, h: 20}},
-  {id: '3', name: 'Option E (14*10)', value:{w: 15, h: 10}}
+  {id: '3', name: 'Option E (14*10)', value:{w: 114, h: 10}}
   ];
 
   $scope.selectedOption = $scope.availableOptions[1];
